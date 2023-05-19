2976b697fddb2c7e31c7357bab2d1aee238603e9
pisuthd <pisuth.dae@gmail.com>
2023-05-19 11:27:40 +0900

    init hardhat project

---
+++ contracts/hardhat.config.js
@@ -0,0 +1,35 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+require("dotenv").config()
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  mocha: {
+    timeout: 1200000,
+  },
+  solidity: {
+    version: "0.8.17",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 1000,
+        details: { yul: false },
+      },
+    },
+  },
+  // defaultNetwork: "hyperspace",
+  // networks: {
+  //   hyperspace: {
+  //     chainId: 3141,
+  //     url: "https://api.hyperspace.node.glif.io/rpc/v1",
+  //     accounts: [PRIVATE_KEY],
+  //   },
+  //   filecoinmainnet: {
+  //     chainId: 314,
+  //     url: "https://api.node.glif.io",
+  //     accounts: [PRIVATE_KEY],
+  //   },
+  // }
+};
