974c00b60d7b83f03cb6059f804c3d3bd3318f09
pisuthd <pisuth.dae@gmail.com>
2023-05-23 15:59:12 +0900

    add docs test case

--- node/test/api.js
+++ node/test/api.js
@@ -68,7 +68,7 @@ describe('#api', () => {
                 });
         });
 
-        it('should get items to the collection success', (done) => {
+        it('should add items to the collection success', (done) => {
 
             const privateKey = process.env.PRIVATE_KEY
             const wallet = new ethers.Wallet(privateKey);
@@ -116,7 +116,45 @@ describe('#api', () => {
             )
         });
 
-        it('should get all items from the collection success', (done) => {
+        it('should add docs to the collection success', (done) => {
+
+            const privateKey = process.env.PRIVATE_KEY
+            const wallet = new ethers.Wallet(privateKey);
+
+            const message = "Sign this message to submit"
+
+            const ids = collections.map(item => item.id)
+            const randomId = ids[Math.floor(Math.random() * ids.length)];
+
+            collectionId = randomId
+
+            wallet.signMessage(message).then(
+                (signature) => {
+
+                    const docs = `
+                    Once upon a time in a small village nestled amidst rolling hills, there lived a poor farmer named Thomas. Thomas was known for his hard work and kind heart. One day, a mischievous fox started wreaking havoc in the village, stealing chickens from the farmers' yards. The villagers grew worried, and their livelihoods were at stake.
+                    `
+
+                    const entry = {
+                        docs,
+                        message,
+                        signature
+                    }
+
+                    chai.request(app)
+                        .post(`/collection/${randomId}`)
+                        .send(entry)
+                        .end((err, res) => {
+                            const { body, status } = res
+                            expect(status).equal(200)
+
+                            done();
+                        });
+                }
+            )
+        });
+
+        it('should get all new items from the collection success', (done) => {
             chai.request(app)
                 .get(`/collection/${collectionId}`)
                 .end((err, res) => {
@@ -138,7 +176,7 @@ describe('#api', () => {
             const q = `${lastItem.pageContent.replace(/[^a-zA-Z0-9 ]/g, '')}`
 
             chai.request(app)
-                .get(`/collection/${collectionId}/search?q=${q}`)
+                .get(`/query/${collectionId}?q=${q}&option=search`)
                 .end((err, res) => {
                     const { body, status } = res
 
