6e9a6d4071901c5038ba36aa5457b03eddb68bc8
pisuthd <pisuth.dae@gmail.com>
2023-05-23 21:30:03 +0900

    fix query button freeze

--- client/src/components/queryCollection.js
+++ client/src/components/queryCollection.js
@@ -98,7 +98,7 @@ const QueryCollection = ({ collectionId, collections }) => {
                 </div>
                 <div class="col-span-4 flex flex-row">
                     <div>
-                        <button disabled={loading || disabled} type="button" onClick={onQuery} class={`${(loading || disabled) && "opacity-60"} text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-2.5 py-1.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700 w-40`}>Query</button>
+                        <button disabled={loading} type="button" onClick={onQuery} class={`${(loading || disabled) && "opacity-60"} text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-2.5 py-1.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700 w-40`}>Query</button>
                     </div>
                     {loading && (
                         <div class="text-sm flex items-center justify-center mb-2 ml-2">
